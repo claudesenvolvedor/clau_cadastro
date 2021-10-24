29879b7afea70b374020d960fca9f95908448f05
clau pinheiro <claudesenvolvedor@gmail.com>
2021-10-24 18:18:10 -0300

    adicionado o arquivo mascarar.js

---
+++ js/mascarar.js
@@ -0,0 +1,6 @@
+//Máscara em alguns campos com somente números digitados
+
+$(function(){ $("#rg").mask("0000000000");});
+$(function(){ $("#cep").mask("00000000");});
+$(function(){ $("#telefone").mask("(00) 0000-0000");});
+$(function(){ $("#celular").mask("(00) 00000-0000");});
